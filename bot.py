544df1e4414142b5a55c4efc35ab904aaa834fb0
XXWolfBane <XXWolfBane@Gmail.com>
2018-07-24 13:47:30 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,6 +10,19 @@ async def on_ready():
     print(bot.user.id)
     print('------')
     
+@bot.command()
+async def ping(ctx):
+    await ctx.send('Pong! My ping is'.format(round(bot.latency, 1) 
+                                             
+                                             
+@bot.command
+async def hug(ctx):
+    await ctx.send('*Hugs ' + discord.message.AuthorUserName + '*')
+
+@bot.command
+async def greet(ctx):
+    await ctx.send("Hello there! :wave:")
+    
 
      
 bot.run('NDcxMDY2MjIzNDcyMjc5NTU0.Djfa0A.Z0jGAYYwwAi5UyzsGj1QqDBUBME')
