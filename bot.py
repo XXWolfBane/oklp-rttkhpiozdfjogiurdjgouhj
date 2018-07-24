1d91e105577dce49ae58e1706f90f983ac20dfcd
XXWolfBane <XXWolfBane@Gmail.com>
2018-07-24 13:41:23 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,14 +10,7 @@ async def on_ready():
     print(bot.user.id)
     print('------')
     
-@bot.command()
-async def createticket(pass_contex=TRUE):
-    r=raident.random
-    await bot.send_message(ctx.message.channel, "You support number is" + str(r) + "please wait for staff to see this."
-    
-@bot.command()
-async def staff.claim(userName: discord.User, ticketNumber: int(a), ctx)
-     await bot.send_message(ctx.message.channel, userName + "Has claimed call number" + ticketNumber)
+
      
 bot.run('NDcxMDY2MjIzNDcyMjc5NTU0.Djfa0A.Z0jGAYYwwAi5UyzsGj1QqDBUBME')
 
